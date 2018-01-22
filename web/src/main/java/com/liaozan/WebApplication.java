9fd1db1874f508fba2a64cf24ca59f0f4339e88a
liaozan <378024053@qq.com>
2018-01-22 23:45:04 +0800

    api docs

--- web/src/main/java/com/liaozan/WebApplication.java
+++ web/src/main/java/com/liaozan/WebApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author liaozan
@@ -12,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
  */
 @SpringBootApplication
 @EnableAsync
+@EnableSwagger2
 @MapperScan(value = "com.liaozan.biz.mapper")
 public class WebApplication {
 
