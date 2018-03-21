0d8651e9a41699b7ef28fa711c7b4c769d8ad32e
liaozan <378024053@qq.com>
2018-03-22 00:01:32 +0800

    update

--- web/src/main/java/com/liaozan/web/ext/MyApplicationListener.java
+++ web/src/main/java/com/liaozan/web/ext/MyApplicationListener.java
@@ -1,9 +1,7 @@
 package com.liaozan.web.ext;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -16,12 +14,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class MyApplicationListener implements ApplicationListener<ApplicationEvent> {
 
-	@Autowired
-	private ApplicationEventPublisher eventPublisher;
-
 	@Override
 	public void onApplicationEvent (ApplicationEvent event) {
-		eventPublisher.publishEvent(event);
 		log.debug("{}", event);
 	}
 }
