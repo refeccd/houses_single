bc12e0b9561d0202988be7658f4f021179c7be8b
liaozan <378024053@qq.com>
2018-05-13 02:46:31 +0800

    update

--- web/src/main/java/com/liaozan/web/ext/MyApplicationListener.java
+++ web/src/main/java/com/liaozan/web/ext/MyApplicationListener.java
@@ -3,7 +3,6 @@ package com.liaozan.web.ext;
 import com.liaozan.web.controller.UserController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
@@ -16,8 +15,8 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class MyApplicationListener implements ApplicationListener<ApplicationReadyEvent> {
 
-	@Override
-	public void onApplicationEvent (ApplicationReadyEvent event) {
-		log.debug("{}", event.getApplicationContext().getBean(UserController.class));
-	}
+    @Override
+    public void onApplicationEvent (ApplicationReadyEvent event) {
+        log.debug("{}", event.getApplicationContext().getBean(UserController.class));
+    }
 }
